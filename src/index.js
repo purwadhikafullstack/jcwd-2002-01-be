13f707525dc6373fe95ccebdc63958f55194c5db
Rickykn <61722793+Rickykn@users.noreply.github.com>
2022-06-13 15:46:16 +0700

    setup express js (#1)
    * setup express cors dotenv
    
    * add .env to git ignore
    
    * delete .env

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("<h1>Final Project API</h1>");
+});
+
+app.listen(PORT, () => {
+  console.log("Listening in port", PORT);
+});
