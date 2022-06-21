fc11d78290deafec8e7c2754d67cc8228346cd92
Rickykn <61722793+Rickykn@users.noreply.github.com>
2022-06-21 15:45:09 +0700

    feat: create api register user

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const authRoutes = require("./auth");
-const userRoutes = require("./user")
+const userRoutes = require("./user");
 
 module.exports = {
   authRoutes,
-  userRoutes
+  userRoutes,
 };
