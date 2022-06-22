8d7376f043afae3ff8d22b67a38748d7c22311dd
Rickykn <61722793+Rickykn@users.noreply.github.com>
2022-06-22 22:51:19 +0700

    fixed setup auth controller

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,25 +1,15 @@
-const authService = require("../services/auth");
+const { authController } = require("../controllers");
 const {
   runValidator,
   rulesValidation,
 } = require("../validation/registerValidation");
 const router = require("express").Router();
 
-router.post("/register", rulesValidation, runValidator, async (req, res) => {
-  try {
-    const serviceResult = await authService.register(req);
-
-    if (!serviceResult.success) throw serviceResult;
-
-    return res.status(serviceResult.statusCode || 201).json({
-      message: serviceResult.message,
-      result: serviceResult.data,
-    });
-  } catch (err) {
-    return res.status(err.statusCode || 500).json({
-      message: err.message,
-    });
-  }
-});
+router.post(
+  "/register",
+  rulesValidation,
+  runValidator,
+  authController.register
+);
 
 module.exports = router;
