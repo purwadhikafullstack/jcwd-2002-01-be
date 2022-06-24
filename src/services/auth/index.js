03c998f9e6dc4f4f6397990070b265f26973afc2
Rickykn <61722793+Rickykn@users.noreply.github.com>
2022-06-24 10:03:44 +0700

    [P1-2] create feature verify user api (#8)
    * setup express cors dotenv
    
    * delete .env
    
    * feat: create api register user
    
    * feat: verify user API
    
    * update env template

--- src/services/auth/index.js
+++ src/services/auth/index.js
@@ -73,5 +73,49 @@ class authService extends Service {
       });
     }
   };
+
+  static VerifyUser = async (req) => {
+    try {
+      const { token } = req.params;
+
+      const findToken = await VerificationToken.findOne({
+        where: {
+          token,
+          is_valid: true,
+          valid_until: {
+            [Op.gt]: moment().utc(),
+          },
+        },
+      });
+
+      if (!findToken) {
+        return this.handleError({
+          message: "Your token is invalid",
+          statusCode: 400,
+        });
+      }
+
+      await User.update(
+        { is_verified: true },
+        {
+          where: {
+            id: findToken.user_id,
+          },
+        }
+      );
+
+      findToken.is_valid = false;
+      findToken.save();
+
+      return this.handleRedirect({
+        link: `http://localhost:3000/verification_page`,
+      });
+    } catch (err) {
+      return this.handleError({
+        message: "Server Error",
+        statusCode: 500,
+      });
+    }
+  };
 }
 module.exports = authService;
