06c9b806463eb229384a506d6aff6630a2175c32
Nizham Mustaqim <nizhammustaqim@gmail.com>
2022-07-01 14:33:13 +0700

    Feat: User can see a product detail page (#15)

---
+++ src/controllers/productController.js
@@ -0,0 +1,38 @@
+const ProductService = require("../services/product");
+
+const productControllers = {
+  getProduct: async (req, res) => {
+    try {
+      const serviceResult = await ProductService.getProduct(req);
+
+      if (!serviceResult.success) throw serviceResult;
+
+      return res.status(serviceResult.statusCode || 200).json({
+        message: serviceResult.message,
+        result: serviceResult.data,
+      });
+    } catch (err) {
+      return res.status(err.statusCode || 500).json({
+        message: err.message,
+      });
+    }
+  },
+
+  getAllProduct: async (req, res) => {
+    try {
+      const serviceResult = await ProductService.getAllProduct(req.query);
+      if (!serviceResult.success) throw serviceResult;
+      return res.status(serviceResult.statusCode || 200).json({
+        message: serviceResult.message,
+        result: serviceResult.data,
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(err.statusCode || 500).json({
+        message: err.message,
+      });
+    }
+  },
+};
+
+module.exports = productControllers;
--- src/services/product/index.js
+++ src/services/product/index.js
@@ -3,6 +3,37 @@ const { Product, ProductImage } = require("../../lib/sequelize");
 const Service = require("../service");
 
 class ProductService extends Service {
+  static getProduct = async (req) => {
+    try {
+      const { productId } = req.params;
+
+      const getProductData = await Product.findOne({
+        where: {
+          id: productId,
+        },
+      });
+
+      if (!getProductData) {
+        return this.handleError({
+          message: `Can't Find Product with ID: ${productId}`,
+          statusCode: 404,
+        });
+      }
+
+      return this.handleSuccess({
+        message: "Product Found",
+        statusCode: 200,
+        data: getProductData,
+      });
+    } catch (err) {
+      console.log(err);
+      return this.handleError({
+        message: "Can't Reach Product",
+        statusCode: 500,
+      });
+    }
+  };
+
   static getAllProduct = async (req) => {
     try {
       const { _limit = 30, _page = 1, _sortBy = "", _sortDir = "" } = req.query;
@@ -45,8 +76,7 @@ class ProductService extends Service {
     }
   };
 
-  static editProduct = async (req) => {
-  };
+  static editProduct = async (req) => {};
 }
 
 module.exports = ProductService;
