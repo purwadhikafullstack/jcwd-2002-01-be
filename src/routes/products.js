03863b12c4a1bdabf94389ee9f2a10fac9f2169c
Nizham Mustaqim <nizhammustaqim@gmail.com>
2022-07-01 14:33:13 +0700

    Feat: User can see a product detail page (#15)

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,23 +1,26 @@
 const ProductService = require("../services/product");
+const productControllers = require("../controllers/productController");
 
 const router = require("express").Router();
 
+router.get("/:productId", productControllers.getProduct);
+
 router.get("/", async (req, res) => {
-    try {
-      const serviceResult = await ProductService.getAllProduct(req);
+  try {
+    const serviceResult = await ProductService.getAllProduct(req);
 
-      if (!serviceResult.success) throw serviceResult;
+    if (!serviceResult.success) throw serviceResult;
 
-      return res.status(serviceResult.statusCode || 201).json({
-        message: serviceResult.message,
-        result: serviceResult.data,
-      });
-    } catch (err) {
-      console.log(err);
-      return res.status(err.statusCode || 500).json({
-        message: err.message,
-      });
-    }
-})
+    return res.status(serviceResult.statusCode || 201).json({
+      message: serviceResult.message,
+      result: serviceResult.data,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(err.statusCode || 500).json({
+      message: err.message,
+    });
+  }
+});
 
-module.exports = router
+module.exports = router;
--- src/services/product/index.js
+++ src/services/product/index.js
@@ -3,6 +3,37 @@ const { Product, ProductImage, Category } = require("../../lib/sequelize");
 const Service = require("../service");
 
 class ProductService extends Service {
+  static getProduct = async (req) => {
+    try {
+      const { productId } = req.params;
+
+      const getProductData = await Product.findOne({
+        where: {
+          id: productId,
+        },
+      });
+
+      if (!getProductData) {
+        return this.handleError({
+          message: `Can't Find Product with ID: ${productId}`,
+          statusCode: 404,
+        });
+      }
+
+      return this.handleSuccess({
+        message: "Product Found",
+        statusCode: 200,
+        data: getProductData,
+      });
+    } catch (err) {
+      console.log(err);
+      return this.handleError({
+        message: "Can't Reach Product",
+        statusCode: 500,
+      });
+    }
+  };
+
   static getAllProduct = async (req) => {
     try {
       const { _limit = 30, _page = 1, _sortBy = "", _sortDir = "", name = "", selectedCategory = ""} = req.query;
