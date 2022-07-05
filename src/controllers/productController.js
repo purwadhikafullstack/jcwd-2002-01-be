4b57fe6a61c87c772a6948afd352a5d552d3c34f
Rickykn <61722793+Rickykn@users.noreply.github.com>
2022-07-05 10:03:18 +0700

    fix code create product and stock

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -51,6 +51,22 @@ const productControllers = {
       });
     }
   },
+
+  createProductStock: async (req, res) => {
+    try {
+      const serviceResult = await ProductService.addStockProduct(req);
+      if (!serviceResult.success) throw serviceResult;
+      return res.status(serviceResult.statusCode || 201).json({
+        message: serviceResult.message,
+        result: serviceResult.data,
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(err.statusCode || 500).json({
+        message: err.message,
+      });
+    }
+  },
 };
 
 module.exports = productControllers;
