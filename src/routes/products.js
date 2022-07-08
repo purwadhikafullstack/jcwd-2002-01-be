c6cba7ae47ed0cba2a09029dd0650abf53646519
Rickykn <61722793+Rickykn@users.noreply.github.com>
2022-07-08 13:41:40 +0700

    [P1-38] [P1-35] fecth product for adim, add product stock admin (#20)
    * fix code create product and stock
    
    * add table stock opname and create api get data for admin
    
    * feat: fix api create stock
    
    * fix clear console.log
    
    * fix code create product and stock
    
    * add table stock opname and create api get data for admin
    
    * feat: fix api create stock
    
    * fix merge with main repo
    
    * fix clean code

--- src/routes/products.js
+++ src/routes/products.js
@@ -61,4 +61,6 @@ router.delete(
 
 router.get("/invenroty/:productId", productController.getInventoryByProductId);
 
+router.get("/quantity", productControllers.getAllProductWithQuantity);
+
 module.exports = router;
