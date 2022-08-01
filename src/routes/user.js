0e3926ac6eb208068403e458c3203485337f5c9e
givennp <98306049+givennp@users.noreply.github.com>
2022-08-01 23:05:18 +0700

    fixed profiling (#37)

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,22 +4,30 @@ const { AuthorizeLoggedInUser } = require("../middlewares/authMiddleware");
 
 const router = require("express").Router();
 
-router.patch("/", userController.editProfile);
+router.patch("/", AuthorizeLoggedInUser, userController.editProfile);
 
 router.patch(
-  "/profile-picture/:id",
+  "/profile-picture",
   fileUploader({
     destinationFolder: "profile-image",
     fileType: "image",
-    prefix: "POST",
+    prefix: "PATCH",
   }).single("profile_image_file"),
+  AuthorizeLoggedInUser,
   userController.editAvatar
 );
 
 router.post("/address", AuthorizeLoggedInUser, userController.addNewAddress);
 router.get("/address", AuthorizeLoggedInUser, userController.getAllAddress);
-router.get("/transaction", AuthorizeLoggedInUser, userController.getAllUserTransaction);
-router.get("/main-address", AuthorizeLoggedInUser, userController.getMainAddress);
-
+router.get(
+  "/transaction",
+  AuthorizeLoggedInUser,
+  userController.getAllUserTransaction
+);
+router.get(
+  "/main-address",
+  AuthorizeLoggedInUser,
+  userController.getMainAddress
+);
 
 module.exports = router;
